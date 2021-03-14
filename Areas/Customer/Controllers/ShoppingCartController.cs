1cc0b2388ff7bbc1c335e792f1e505b0387ea038
Dmytro Skorobagatyi <Skorobagatyi.Dmitriy@Gmail.com>
2021-03-15 01:13:57 +0200

    [HttpPost] Index Method Added

--- Areas/Customer/Controllers/ShoppingCartController.cs
+++ Areas/Customer/Controllers/ShoppingCartController.cs
@@ -10,6 +10,7 @@ using WebStore.Models.ViewModel;
 
 namespace WebStore.Areas.Customer.Controllers
 {
+    [Area(nameof(Customer))]
     public class ShoppingCartController : Controller
     {
         private readonly ApplicationDbContext _db;
@@ -24,20 +25,53 @@ namespace WebStore.Areas.Customer.Controllers
             };
         }
         [HttpGet]
-        public async Task <IActionResult> Index(int id)
+        public async Task <IActionResult> Index()
         {
             List<int> listOfShoppingCart = HttpContext.Session.Get<List<int>>("sShoppingCart");
+
             if (listOfShoppingCart.Count > 0)
             {
-                foreach(var item in listOfShoppingCart)
+                foreach(var cartItem in listOfShoppingCart)
                 {
                    Product product = await _db.Products.Include(x => x.ProductTypes)
                                                        .Include(x => x.SpecialTags)                                                        
-                                                       .FirstOrDefaultAsync(x=>x.Id==id);
-                   // ShoppingCartVM.Products = product;
+                                                       .FirstOrDefaultAsync(x=>x.Id == cartItem);
+
+                   ShoppingCartVM.Products.Add(product);
                 }              
             }
             return View(ShoppingCartVM);
+        }
+        [HttpPost]
+        [ActionName("Index")]
+        public async Task<IActionResult> IndexPost()
+        {
+            List<int> listOfShoppingCart = HttpContext.Session.Get<List<int>>("sShoppingCart");     
+            
+            ShoppingCartVM.Appointment.AppointmentDay=ShoppingCartVM.Appointment.AppointmentDay.AddHours(0.00).AddMinutes(0.00);
+            Appointment appointment = ShoppingCartVM.Appointment;
+
+            _db.Appointments.Add(appointment);
+            await _db.SaveChangesAsync();
+            
+            var id = await _db.Appointments.FirstOrDefaultAsync(x => x.Id == ShoppingCartVM.Appointment.Id);
+
+            foreach(var cartItem in listOfShoppingCart)
+            {
+                ProductsForAppointment productsForAppointment = new();
+                productsForAppointment.Products = await _db.Products.Include(x => x.ProductTypes)
+                                                                    .Include(x => x.SpecialTags)
+                                                                    .FirstOrDefaultAsync(x => x.Id == cartItem);
+
+                productsForAppointment.Appointments = await _db.Appointments.FirstOrDefaultAsync(x => x.Id == cartItem);
+
+                _db.ProductsForAppointments.Add(productsForAppointment);
+            }
+            await _db.SaveChangesAsync();
+            List<int> listOfProducts = new List<int>();
+            listOfShoppingCart = listOfProducts;
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
